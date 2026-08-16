bdca60a5ee2c0f32976feac5d955c3bac987517c
ufisigg-safinah <ufisigg@gmail.com>
2026-08-16 21:15:19 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ import {
 import {
     collection,
     getDocs
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 
 // =====================================
