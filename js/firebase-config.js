bd78e1b326d5b3e9caa1c8b62e874eb6d6785c5f
ufisigg-safinah <ufisigg@gmail.com>
2026-08-16 20:14:47 +0700

    Update Firebase config to use newer SDK version

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,52 +1,81 @@
-// Firebase App
-import {
-    initializeApp
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+// ==========================================
+// FIREBASE CONFIG
+// SAFINATUSSURUR
+// ==========================================
 
-// Firebase Authentication
 import {
-    getAuth
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+    initializeApp,
+    getApps
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
-// Firestore
 import {
     getFirestore
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+
+// ==========================================
+// CONFIG
+// ==========================================
 
 const firebaseConfig = {
 
-    apiKey: "AIzaSyD0izNMCi-Ef52v_bW5WWeB4nxoaUrehG4",
+    apiKey:
+        "AIzaSyD0izNMCi-Ef52v_bW5WWeB4nxoaUrehG4",
 
-    authDomain: "safinatussurur.firebaseapp.com",
+    authDomain:
+        "safinatussurur.firebaseapp.com",
 
-    projectId: "safinatussurur",
+    projectId:
+        "safinatussurur",
 
-    storageBucket: "safinatussurur.firebasestorage.app",
+    storageBucket:
+        "safinatussurur.firebasestorage.app",
 
-    messagingSenderId: "1065917297456",
+    messagingSenderId:
+        "1065917297456",
 
-    appId: "1:1065917297456:web:27197bcdee49c226920ef5",
+    appId:
+        "1:1065917297456:web:27197bcdee49c226920ef5"
 
-    measurementId: "G-CPHKKWHZRD"
 };
 
 
-// Inisialisasi Firebase
-const app = initializeApp(firebaseConfig);
+// ==========================================
+// INITIALIZE APP
+// ==========================================
+
+const app =
+    getApps().length > 0
+        ? getApps()[0]
+        : initializeApp(firebaseConfig);
 
 
-// Firebase Authentication
-const auth = getAuth(app);
+// ==========================================
+// FIRESTORE
+// ==========================================
+
+const db =
+    getFirestore(app);
 
 
-// Firestore
-const db = getFirestore(app);
+// ==========================================
+// AUTH
+// ==========================================
+
+const auth =
+    getAuth(app);
 
 
-// Export
+// ==========================================
+// EXPORT
+// ==========================================
+
 export {
     app,
-    auth,
-    db
+    db,
+    auth
 };
